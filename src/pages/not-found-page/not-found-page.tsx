0c569571907169148f326f2409aac8f7b4259fe7
Anastasiya Alekseeva <anastasiyaalexxx@gmail.com>
2026-05-26 13:54:58 +0300

    Правки #3

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
 
 function NotFoundPage() {
   return (
@@ -7,7 +8,7 @@ function NotFoundPage() {
         <div className="container">
           <h1>404 Not Found</h1>
           <p>Page not found.</p>
-          <Link to="/">Go to main page</Link>
+          <Link to={AppRoute.Main}>Go to main page</Link>
         </div>
       </main>
     </div>
