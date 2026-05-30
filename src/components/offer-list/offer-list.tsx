8936795de36a301c37a69df232d3d37546ee1607
Anastasiya Alekseeva <anastasiyaalexxx@gmail.com>
2026-05-30 16:53:46 +0300

    Добавляет моки и отрисовку предложений

---
+++ src/components/offer-list/offer-list.tsx
@@ -0,0 +1,25 @@
+import { Offer } from '../../types/offer';
+import OfferCard from '../offer-card/offer-card';
+
+type OffersListProps = {
+  offers: Offer[];
+  onCardMouseEnter: (offerId: string) => void;
+  onCardMouseLeave: () => void;
+};
+
+function OffersList({offers, onCardMouseEnter, onCardMouseLeave}: OffersListProps) {
+  return (
+    <div className="cities__places-list places__list tabs__content">
+      {offers.map((offer) => (
+        <OfferCard
+          key={offer.id}
+          offer={offer}
+          onMouseEnter={onCardMouseEnter}
+          onMouseLeave={onCardMouseLeave}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default OffersList;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/app';
-import { Setting } from './const';
+import { offers } from './mocks/offers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,6 +9,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App offersCount={Setting.OffersCount} />
+    <App offers={offers} />
   </React.StrictMode>
 );
